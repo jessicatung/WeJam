93dd565a58c09a696fe24f7fad2040339d7398bd
Sandra & Jessica Tung, Slayer of Gods, Destroyer of Worlds <pair+bobandraa.jessicatung@devbootcamp.com>
2014-09-25 16:03:00 -0700

    Add Musician model and migration -add validations of presence and uniqueness to model

---
+++ WeJam/db/migrate/20140925225344_create_musicians.rb
@@ -0,0 +1,19 @@
+class CreateMusicians < ActiveRecord::Migration
+  def change
+    create_table :musicians do |t|
+      t.string :username
+      t.string :password_hash
+      t.string :email
+      t.string :location
+      t.string :instrument
+      t.string :genre
+      t.string :skill_level
+      t.string :url
+      t.string :gravatar_url
+      t.text :notes
+      t.boolean :available
+
+      t.timestamps
+    end
+  end
+end
