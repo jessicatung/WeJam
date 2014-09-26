2545c742c6bca907eec93e218a22065681c03c24
minedamnesia <mrsyamamoto2007@gmail.com>
2014-09-25 17:43:12 -0700

    add has_secure_password to model to support bcrypt

--- WeJam/app/models/musician.rb
+++ WeJam/app/models/musician.rb
@@ -1,4 +1,5 @@
 class Musician < ActiveRecord::Base
+  has_secure_password
   validates :username, :email, :password_hash, :location, :instrument, :skill_level, :available, presence: true
   validates :email, :username, uniqueness: true
 end
