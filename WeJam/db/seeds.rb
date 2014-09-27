2590b79e619996865adf9ca386630d41912facb0
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-27 16:39:39 -0700

    Remove erroneous WeJam/db folder - When changing our repo to be non-nested, we somehow left this file behind.

--- WeJam/db/seeds.rb
+++
@@ -1,8 +0,0 @@
-require 'faker'
-
-def seed
- 10.times {Musician.create(username: Faker::Internet.user_name, password_hash: "password", email: Faker::Internet.email, location: "Oakland", instrument: Faker::Lorem.word, genre: Faker::Lorem.word, skill_level: Faker::Lorem.word, url: Faker::Internet.url, gravatar_url: Faker::Internet.url, notes: Faker::Lorem.sentence, available: [false, true].sample)}
-
-end
-
-seed
