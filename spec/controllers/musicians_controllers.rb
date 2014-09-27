434acc074ff04fb0df91228604b1a6cb3706a967
Kelly Yamamoto <mrsyamamoto2007@gmail.com>
2014-09-26 17:11:14 -0700

    Update musicians_controllers.rb

--- spec/controllers/musicians_controllers.rb
+++ spec/controllers/musicians_controllers.rb
@@ -14,26 +14,26 @@ describe MusiciansController, :type => :controller do
     end
   end
 
-	context "#create" do 
-		it "should create a new musician" do 
-			expect{
-				post :create, musician: attributes_for(:musician)
-			}.to change{Musician.count}.by(1)
-		end
+  context "#create" do 
+    it "should create a new musician" do 
+      expect{
+        post :create, musician: attributes_for(:musician)
+        }.to change{Musician.count}.by(1)
+    end
     it "returns successful status" do
       post :create, :musician => attributes_for(:musician)
       expect(response.status).to eq(302)
     end
-		it "doesnt create a user with invalid params" do
-			expect {
-			post :create, musician: {username: nil, password: nil}
-			}.to_not change { Musician.count }
-		end
-		it "should redirect to the main index page" do 
-			expect {
-				post :create, musician: attributes_for(:musician)
-				should redirect_to musicians_path
-			}
-		end
-	end
+    it "doesnt create a user with invalid params" do
+      expect {
+        post :create, musician: {username: nil, password: nil}
+        }.to_not change { Musician.count }
+    end
+    it "should redirect to the main index page" do 
+      expect {
+        post :create, musician: attributes_for(:musician)
+        should redirect_to musicians_path
+      }
+    end
+  end
 end
