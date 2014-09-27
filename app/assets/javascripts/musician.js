c7e1d8ddbfedfa061b5217dd45017f6b987940a4
bobandraa <sandra.cohen.ux@gmail.com>
2014-09-26 18:17:03 -0700

    added basic marker

--- app/assets/javascripts/musician.js
+++ app/assets/javascripts/musician.js
@@ -5,17 +5,25 @@ var map;
 // initializing map function
 function initialize (location){
 	console.log(location);
-
+	// assigning new google map obj to currentLocation var to reuse throughout the code
+	var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
 	var mapOptions = {
-	  center: new google.maps.LatLng(37.7843796, -122.39711),
-	  zoom: 15
-		};
+		center: currentLocation,
+		zoom: 15
+	};
 
 // Map takes 2 params (page elements, and map object)
 
-		var map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
+	map = new google.maps.Map(document.getElementById("map-canvas"),
+	mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: currentLocation,
+      map: map
+  });
 }
+
+
 
 $(document).ready(function(){
 	navigator.geolocation.getCurrentPosition(initialize);
