d00edb96338600443e5c2e10b9ca353ef036844c
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-28 21:32:16 -0700

    Add ajax request for setting user location and getting nearby musicians to MapController prototype - separate out the success and fails of ajax call

--- app/assets/javascripts/MapController.js
+++ app/assets/javascripts/MapController.js
@@ -6,6 +6,23 @@ function MapController(userPosition, view){
 
 MapController.prototype = {
 	init: function(){
-		this.view.init(this.userPosition.coords.latitude, this.userPosition.coords.longitude)
+		this.view.init(this.userPosition.coords.latitude, this.userPosition.coords.longitude);
+		this.setUserLocationAndGetNearbyMusicians();
+	},
+	setUserLocationAndGetNearbyMusicians: function(){
+		var ajaxRequest = $.ajax({
+			url: "/musicians/set_location", 
+			method: "POST",
+			data: {lat: this.userPosition.coords.latitude, long: this.userPosition.coords.longitude}
+		})
+		ajaxRequest.done(this.getNearbyMusiciansSuccess.bind(this))
+		ajaxRequest.fail(this.getNearbyMusiciansFail.bind(this))
+	},
+	getNearbyMusiciansSuccess: function(response){
+		console.log(this)
+		this.view.dropHellaMarkers(response)
+	},
+	getNearbyMusiciansFail: function(){
+		alert("Sorry, that didn't work!")
 	}
 }
