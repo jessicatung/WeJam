68b8ab06818c498bd1385ca289ef3a14e1f248ec
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-29 03:09:53 -0700

    Fix seed so that one marker doesn't overlap signed in/logged in user

--- app/assets/javascripts/MapController.js
+++ app/assets/javascripts/MapController.js
@@ -6,6 +6,7 @@ function MapController(view){
 MapController.prototype = {
 	init: function(userPosition){
 		this.view.init(userPosition.coords.latitude, userPosition.coords.longitude);
+		console.log(userPosition.coords.latitude, userPosition.coords.longitude)
 	},
 	showMarkers: function(response){
 		this.view.dropHellaMarkers(response)
