0c97ffa3c24754f2bc6d25936a97ff2ec7db6858
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-28 18:25:02 -0700

    Add drop marker functionality - define currentLocation so can use throughout MapView prototype - implement dropMarker function

--- app/assets/javascripts/MapView.js
+++ app/assets/javascripts/MapView.js
@@ -1,14 +1,22 @@
 function MapView(){
-
+	this.map // there is space for a map now! 
 }
 
 MapView.prototype = {
 	init: function(userLat, userLong){
+		var currentLocation = new google.maps.LatLng(userLat, userLong);
 		var mapOptions = {
-			center: new google.maps.LatLng(userLat, userLong),
+			center: currentLocation,
 			zoom: 15
 		}
-		var map = new google.maps.Map(document.getElementById("map-canvas"),
+		this.map = new google.maps.Map(document.getElementById("map-canvas"),
   	mapOptions);
+  	this.dropMarker(currentLocation);
+	},
+	dropMarker: function(currentLocation){
+		var marker = new google.maps.Marker({
+			position: currentLocation,
+			map: this.map
+		});
 	}
 }
