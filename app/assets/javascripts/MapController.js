92d0d2007661d0fa9a41163735fed4d0c8568c69
Jessica Tung <jessicatung@Jessicas-MacBook-Pro.local>
2014-09-28 17:24:14 -0700

    Pass geolocation based user lat/long to MapView

--- app/assets/javascripts/MapController.js
+++ app/assets/javascripts/MapController.js
@@ -1,10 +1,11 @@
 function MapController(userPosition, view){
 	this.userPosition = userPosition;
+	console.log(userPosition)
 	this.view = view;
 }
 
 MapController.prototype = {
 	init: function(){
-		this.view.init(userPosition.coords.latitude, userPosition.coords.longitude)
+		this.view.init(this.userPosition.coords.latitude, this.userPosition.coords.longitude)
 	}
 }
