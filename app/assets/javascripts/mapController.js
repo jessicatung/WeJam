54c43c01cf31708eabce04382d642c27836ebb36
minedamnesia <mrsyamamoto2007@gmail.com>
2014-09-30 12:46:47 -0700

    updates which add new wejam controller (containing musicians controller); interim commit of mapcontroller files

--- app/assets/javascripts/mapController.js
+++ app/assets/javascripts/mapController.js
@@ -1,54 +1,34 @@
 //init with userLocation and musicians array
 
-function MapController(view, userPosition, musiciansController){
-	this.musicianArray = musiciansController
-	this.view = new MapView(userPosition, musicianArray)
-	this.map // there is space for a map now! 
-	this.droppedMarkers = []
-	this.dropMarker()
-	this.dropHellaMarkers()()
+function MapController(selector, mapOptions){
+	this.map = new google.maps.Map(document.getElementById(selector), mapOptions)
 }
 
 MapController.prototype = {
-	init: function(userPosition){
-		this.view.init(userPosition.coords.latitude, userPosition.coords.longitude);
+	drawMapWithMusicians: function(pinables, specificPinable){
+		console.log(specificPinable)
+		this._dropSpecificPin(specificPinable);
+		this._dropPinables(pinables);
 	},
-	showMarkers: function(response){
-		this.view.dropHellaMarkers(response)
+	_dropSpecificPin: function(specificPinable){
+		console.log(specificPinable)
+		// var specificPinableLatArray = this._mapIntoGoogleLatLong(pinableObjects)
+		// new google.maps.Marker({
+		// 	position: specificPinableLatArray[0],
+		// 	map: this.map,
+		// 	marker: specificPinable.id
+		// })
 	},
-	bindMarkerListeners: function(musicianController){
-		for (var i = 0; i < this.view.droppedMarkers.length; i++){
-			// pass it what obj it's listening to, what evt listening for, callback
-      google.maps.event.addListener(this.view.droppedMarkers[i], 'click', function(innerKey) {
-        // this is the particular dropped marker
-        musicianController.showMusiciansProfile(this)
-      });
-    }
+
+	_mapIntoGoogleLatLong:function(pinableObjects){
+		var latLongArray = []
+		for (var i = 0; i < pinableObjects.length; i++){
+			console.log(pinableObjects[i])
+			var lat = pinableObjects[i].lat
+			var long = pinableObjects[i].long
+			var googleLatLng = new google.maps.LatLng(lat, long)
+			latLongArray.push(googleLatLng)
+		}
 	}
 }
 
-MapView.prototype = {
-	init: function(userLat, userLong){
-		var currentLocation = new google.maps.LatLng(userLat, userLong);
-		var mapOptions = {
-			center: currentLocation,
-			zoom: 15
-		}
-		this.map = new google.maps.Map(document.getElementById("map-canvas"),
-  	mapOptions);
-  	this.dropMarker(currentLocation);
-	},
-	dropMarker: function(location, markerId){
-		var marker = new google.maps.Marker({
-			position: location,
-			map: this.map,
-			markerIdentifier: markerId
-		})
-		this.droppedMarkers.push(marker)
-	},
-	dropHellaMarkers: function(locationCollection){
-		for (var i=0; i<locationCollection.length; i++){
-			this.dropMarker(new google.maps.LatLng(locationCollection[i].lat, locationCollection[i].long), locationCollection[i].id)
-		}	
-	}
-}
--- app/assets/javascripts/wejam.js
+++ app/assets/javascripts/wejam.js
@@ -4,14 +4,15 @@ $(document).ready(function(){
 
 WeJam = {
 	initialize: function(){
-		console.log('here i am initializing');
 		navigator.geolocation.getCurrentPosition(this.getCoordinatesSuccess, this.getCoordinatesFailure)
 	},
 	getCoordinatesSuccess: function(userPosition){
 		this.userPosition = userPosition;
-
-		musiciansController = new MusiciansController(userPosition);
-		mapController = new MapController(view, userPosition, musiciansController);
+		mapController = new MapController("map-canvas",{
+			center: new google.maps.LatLng(this.userPosition.coords.latitude, this.userPosition.coords.longitude),
+			zoom: 15
+		});
+		musiciansController = new MusiciansController(userPosition, mapController);
 	},
 	getCoordinatesFailure: function(){
 		alert("We can't find you! Still looking.")
@@ -19,25 +20,24 @@ WeJam = {
 	}
 }
 
-function MusiciansController(userPosition){
+function MusiciansController(userPosition, mapController){
+	this.currentMusician = null;
+	this.musicianArray = [];
 
-	this.musicianArray =[];
 	this.fetchCurrentMusician();
-	this.setMyLocation(userPosition);
 	this.fetchMusicians();
+	this.setMyLocation(userPosition);
+	mapController.drawMapWithMusicians(this.musicianArray, this.currentMusician);
 }	
 
 MusiciansController.prototype={
 //dependencies: needs intial geolocation	
 	fetchCurrentMusician: function(){
-		c = this;
 		$.ajax({
 			url: '/musicians/show_me',
 			method: 'GET'
 		}).done(function(serverData){
-			debugger
-			c.currentMusician = this.currentMusician = new Musician(serverData)
-
+			this.currentMusician = new Musician(serverData)
 		}.bind(this)).fail(function(serverData){
 			console.log("fail")
 		})
@@ -60,32 +60,11 @@ MusiciansController.prototype={
 		}).done(function(serverData){
 			for (var i = 0; i < serverData.length; i++){
 				var response = serverData[i]
-				musicianArray.push(response)
+				this.musicianArray.push(new Musician(response))
 			};
-		}).fail(function(serverData){
+		}.bind(this)).fail(function(serverData){
 			console.log("fail")
 		})
 	},
 
-// function Renderer(item_to_render){
-// 	this.template = "<div class=\"show_container\"><h1>" + musician.username + 
-//     "</h1><p>Location: " + musician.location + 
-//     "</p><p>Instrument: " + musician.instrument + 
-//     "</p><p>Genre: " + musician.genre +
-//     "</p><p>Skill Level: " + musician.skillLevel + 
-//     "</p><p>Soundcloud/YouTube Link: <a href=\"" + musician.url + 
-//     "\">" + musician.url + 
-//     "</a></p><p>Pic Link:  <a href=\"" + musician.gravatarUrl + 
-//     "\">" + musician.gravatarUrl + 
-//     "</a></p><p>About Me: " + musician.notes + 
-//     "</p> <p> <a href=\"mailto:" + musician.email + 
-//     "\">Email Me!</a></p></div>"
-// 	console.log(item_to_render)	
-// }
-
-// Renderer.prototype={
-// 	profile: function(musician, template){
-// 		(".container").append(template(musician))
-// 	}
-// }
 }
