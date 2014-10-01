f035c757d085eb80df49434f36be802034cd6fc5
Matthew Darin <mgd2233@gmail.com>
2014-09-30 23:03:44 -0700

    setup jasmine tests

---
+++ spec/javascripts/wejam_spec.js
@@ -0,0 +1,30 @@
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+    return fBound;
+  };
+}
+
+describe("We jam specs", function(){
+  beforeEach(function(){
+    // speedTest = new ReddyReader.SpeedTest();
+  });
+   it("recieves a paragraph to display from the server", function(){
+
+  });
+
+  
+})
